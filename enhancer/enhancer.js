bf67ad58f5b191b01d1dd524996c82855847619f
bhumip214 <bhumip214@gmail.com>
2019-03-04 18:25:23 -0800

    success function and test done

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,7 +1,36 @@
-const success = item => {};
+// The item's enhancement increases by 1.
+//The name is updated to reflect the new enhancement level.
+const success = item => {
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1;
 
+    if (item.enhancement <= 15) {
+      item.displayName = `[+${item.enhancement}] ${item.name}`;
+    } else if (item.enhancement === 16) {
+      item.displayName = `[PRI] ${item.name}`;
+    } else if (item.enhancement === 17) {
+      item.displayName = `[DUO] ${item.name}`;
+    } else if (item.enhancement === 18) {
+      item.displayName = `[TRI] ${item.name}`;
+    } else if (item.enhancement === 19) {
+      item.displayName = `[TET] ${item.name}`;
+    } else if (item.enhancement === 20) {
+      item.displayName = `[PEN] ${item.name}`;
+    }
+    return item;
+  }
+  return item;
+};
+
+//The durability of the item is decreased by 5 if the item's enhancement is between 0 and 14.
+// The durability of the item is decreased by 10 if the item's enhancement is greater than 14.
+// If the item's enhancement level is greater than 16 (DUO, TRI, TET), the enhancement level decreases by 1 (a DUO item would go back to PRI on failure).
+// The name is updated to reflect the new enhancement level if it was decreased.
+// If the item's enhancement is 14 or lower, the item cannot be enhanced if the durability is below 25.
+// If the item's enhancement is 15 or higher, the item cannot be enhanced if the durability is below 10.
 const fail = item => {};
 
+//durability restored to 100
 const repair = item => {
   return {
     ...item,
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,12 +1,32 @@
-const { repair } = require("./enhancer.js");
+const { success, repair } = require("./enhancer.js");
 
-it("repairs to full durability", () => {
-  const item = {
-    name: "Sword",
-    type: "Weapon",
-    durability: 98,
-    enchancement: 15
-  };
+describe("enhancer.js", () => {
+  describe("success()", () => {
+    it("increases enchancement by one & updated name", () => {
+      const item = {
+        name: "Shield",
+        type: "Armor",
+        durability: 100,
+        enhancement: 19,
+        displayName: "[+19] Shield"
+      };
 
-  expect(repair(item).durability).toBe(100);
+      const result = success(item);
+      expect(result.enhancement).toBe(20);
+      expect(result.displayName).toBe("[PEN] Shield");
+    });
+  });
+
+  describe("repair()", () => {
+    it("repairs to full durability", () => {
+      const item = {
+        name: "Sword",
+        type: "Weapon",
+        durability: 98,
+        enchancement: 15
+      };
+
+      expect(repair(item).durability).toBe(100);
+    });
+  });
 });
